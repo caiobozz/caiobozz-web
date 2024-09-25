54f19a4276d33406756d81e8e38ba71b6fef6185
caiobozz <caiofragabozzetti@gmail.com>
2024-09-25 18:30:44 -0300

    criado troca de tema e adicionado icones MDI

--- src/app/components/main-navbar/main-navbar.component.ts
+++ src/app/components/main-navbar/main-navbar.component.ts
@@ -4,8 +4,17 @@ import { Component } from '@angular/core';
   selector: 'main-navbar',
   standalone: true,
   imports: [],
-  templateUrl: './main-navbar.component.html'
+  templateUrl: './main-navbar.component.html',
 })
 export class MainNavbarComponent {
-
+  public theme: string = 'dark';
+  public changeTheme() {
+    if (this.theme == 'dark') {
+      this.theme = 'light';
+      document.documentElement.setAttribute('data-bs-theme', this.theme);
+    } else {
+      this.theme = 'dark';
+      document.documentElement.setAttribute('data-bs-theme', this.theme);
+    }
+  }
 }
